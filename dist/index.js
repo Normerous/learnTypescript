c539d3c976bd82eda1528a6a494c72f9997a977d
Normerous <k.warayout@gmail.com>
2022-06-10 22:34:28 +0700

    learn: Literal Type

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,5 @@
 "use strict";
+var _a;
 // Static type
 let age = 20;
 if (age < 50)
@@ -18,23 +19,66 @@ myArray[3] = undefined;
 myArray[4] = () => { };
 console.log(myArray);
 // User-Defined DataType ---> array, Class, Touple, Enums, Interface, Functions
-const myFC1 = () => { };
-function myFC2(input) { }
 // Tuples
-let user = [1, 'string'];
-user.push('wrong'); // waring !!!!!!!!!
+let user = [1, "string"];
+user.push("wrong"); // waring !!!!!!!!!
 console.log("user", user);
 // Enum member must have initializer
 var Size;
 (function (Size) {
-    console.log('size', Size)
     Size[Size["Small"] = 1] = "Small";
     Size[Size["Medium"] = 2] = "Medium";
     Size[Size["Large"] = 3] = "Large";
 })(Size || (Size = {}));
-console.log(Size)
 let mySize = Size.Medium;
 console.log(mySize);
 let mySize2 = 4 /* Size2.Large */;
 console.log(mySize2);
+// functions
+const myFunc1 = () => { };
+function myFunc2(input, input2 = 2020) {
+    if (input2 <= 2020)
+        return input * 1.1;
+    return input * 1;
+}
+myFunc2(100);
+myFunc2(200, 2021);
+console.log("1");
+setTimeout(() => {
+    console.log("2");
+}, 0);
+console.log("3");
+let emp = {
+    id: 1,
+    name: "Tum",
+    fn: (date) => {
+        console.log(date);
+    },
+};
+function kgToLb(weight) {
+    if (typeof weight === "number")
+        return weight * 2.2;
+    else
+        return parseInt(weight) * 2.2;
+}
+console.log(kgToLb(10));
+console.log(kgToLb("10kg"));
+let textBox = {
+    drag: () => {
+        console.log("..xxxXX");
+    },
+    resize: () => {
+        console.log("..zzzZZ");
+    },
+};
+textBox.drag();
+textBox.resize();
+let quantity = 100;
+let quantity1;
+quantity1 = 20;
+function getCustomer(id) {
+    return id === 0 ? null : { birthday: new Date() };
+}
+let customer = getCustomer(1);
+console.log((_a = customer === null || customer === void 0 ? void 0 : customer.birthday) === null || _a === void 0 ? void 0 : _a.getFullYear());
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -19,25 +19,102 @@ myArray[4] = (): void => {};
 console.log(myArray);
 // User-Defined DataType ---> array, Class, Touple, Enums, Interface, Functions
 // Tuples
-let user: [number, string] = [1, 'string']
-user.push('wrong') // waring !!!!!!!!!
-console.log("user", user)
+let user: [number, string] = [1, "string"];
+user.push("wrong"); // waring !!!!!!!!!
+console.log("user", user);
 
 // Enum member must have initializer
-enum Size { Small = 1, Medium, Large }
-let mySize = Size.Medium
-console.log(mySize)
+enum Size {
+  Small = 1,
+  Medium = 2,
+  Large = 3,
+}
+let mySize = Size.Medium;
+console.log(mySize);
 
-const enum Size2 { Small = 3, Medium = 3, Large  }
-let mySize2 = Size2.Large
-console.log(mySize2)
+const enum Size2 {
+  Small = 3,
+  Medium = 3,
+  Large,
+}
+let mySize2 = Size2.Large;
+console.log(mySize2);
+
 // functions
 const myFunc1 = (): void => {};
 function myFunc2(input: number, input2 = 2020): number {
-    if(input2 <= 2020)
-        return input * 1.1
-    return input * 1
+  if (input2 <= 2020) return input * 1.1;
+  return input * 1;
 }
 
-myFunc2(100)
-myFunc2(200, 2021)
+myFunc2(100);
+myFunc2(200, 2021);
+
+console.log("1");
+setTimeout(() => {
+  console.log("2");
+}, 0);
+console.log("3");
+
+type Employee = {
+  readonly id: number;
+  name: string;
+  fn: (date: Date) => void;
+};
+
+let emp: Employee = {
+  id: 1,
+  name: "Tum",
+  fn: (date: Date) => {
+    console.log(date);
+  },
+};
+
+function kgToLb(weight: number | string): number {
+  if (typeof weight === "number") return weight * 2.2;
+  else return parseInt(weight) * 2.2;
+}
+
+console.log(kgToLb(10));
+console.log(kgToLb("10kg"));
+
+// Intersection Types ***************
+type Draggable = {
+  drag: () => void;
+};
+
+type Resizeable = {
+  resize: () => void;
+};
+
+type UIWidget = Draggable & Resizeable;
+
+let textBox: UIWidget = {
+  drag: () => {
+    console.log("..xxxXX");
+  },
+  resize: () => {
+    console.log("..zzzZZ");
+  },
+};
+
+textBox.drag();
+textBox.resize();
+
+// Literal Types ******************
+type Quantity = 50 | 100;
+let quantity: Quantity = 100;
+let quantity1: 10 | 20;
+quantity1 = 20;
+
+// Optional Chaining **********
+type Customer = {
+  birthday: Date;
+};
+
+function getCustomer(id: number): Customer | null {
+  return id === 0 ? null : { birthday: new Date() };
+}
+
+let customer = getCustomer(1);
+console.log(customer?.birthday?.getFullYear());
