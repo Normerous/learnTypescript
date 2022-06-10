f95ea65296d7201cdcb7b769866cd512c9511a53
Normerous <k.warayout@gmail.com>
2022-06-10 09:12:41 +0700

    learn: type

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,6 @@
 "use strict";
 // Static type
 let age = 20;
-console.log("age::", age);
 if (age < 50)
     age += 10;
 console.log(age);
@@ -11,17 +10,31 @@ number.fill(10, 0, 2);
 console.log(number);
 console.log(number[4]);
 let myArray = [];
-// Built-in have 5 type
+// Built-in have 5 type --->>> number, string, null, undefined, void
 myArray[0] = 1;
 myArray[1] = "string";
 myArray[2] = null;
 myArray[3] = undefined;
 myArray[4] = () => { };
 console.log(myArray);
+// User-Defined DataType ---> array, Class, Touple, Enums, Interface, Functions
 const myFC1 = () => { };
 function myFC2(input) { }
 // Tuples
 let user = [1, 'string'];
 user.push('wrong'); // waring !!!!!!!!!
 console.log("user", user);
+// Enum member must have initializer
+var Size;
+(function (Size) {
+    console.log('size', Size)
+    Size[Size["Small"] = 1] = "Small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {}));
+console.log(Size)
+let mySize = Size.Medium;
+console.log(mySize);
+let mySize2 = 4 /* Size2.Large */;
+console.log(mySize2);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -10,17 +10,34 @@ console.log(number);
 console.log(number[4]);
 
 let myArray: any[] = [];
-// Built-in have 5 type
+// Built-in have 5 type --->>> number, string, null, undefined, void
 myArray[0] = 1;
 myArray[1] = "string";
 myArray[2] = null;
 myArray[3] = undefined;
 myArray[4] = (): void => {};
 console.log(myArray);
-
-const myFC1 = (): void => {};
-function myFC2(input: any): void {}
+// User-Defined DataType ---> array, Class, Touple, Enums, Interface, Functions
 // Tuples
 let user: [number, string] = [1, 'string']
 user.push('wrong') // waring !!!!!!!!!
 console.log("user", user)
+
+// Enum member must have initializer
+enum Size { Small = 1, Medium, Large }
+let mySize = Size.Medium
+console.log(mySize)
+
+const enum Size2 { Small = 3, Medium = 3, Large  }
+let mySize2 = Size2.Large
+console.log(mySize2)
+// functions
+const myFunc1 = (): void => {};
+function myFunc2(input: number, input2 = 2020): number {
+    if(input2 <= 2020)
+        return input * 1.1
+    return input * 1
+}
+
+myFunc2(100)
+myFunc2(200, 2021)
